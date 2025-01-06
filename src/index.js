010a4b9f593a438f87f83a757cea7c241f35ffc7
jbk2 <james@humanbenefits.com>
2025-01-06 11:46:28 +0100

    Import date-fns, create TodoItem class
    - npm install date-fns and import into index.js
    - create todo-item.js and write a TodoItem class with
      private properties and their getters and setters.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './assets/stylesheets/styles.css'
+import { format } from "date-fns";
+import TodoItem from "./todo-item.js";
 // import './assets/fonts/*'
 // import './assets/images/*'
+
+// NOTES
+// Projects have ToDo Items
+// ToDo Items have:
+
+
+
+let date = new Date();
+let formattedDate = format(date, "dd/MM/yyyy")
+console.log(formattedDate);
+
+const td1 = new TodoItem("Eggs", "basdf", date, false, false)
+
+console.log(td1);
---
+++ src/todo-item.js
@@ -0,0 +1,67 @@
+class TodoItem {
+  #title;
+  #description;
+  #dueDate;
+  #priority;
+  #done;
+
+  constructor(title, description, dueDate, priority, done) {
+    this.setTitle(title);
+    this.setDescription(description);
+    this.setDueDate(dueDate);
+    this.setPriority(priority);
+    this.setDone(done);
+  }
+
+  getTitle() {
+    return this.#title;
+  }
+  setTitle(title) {
+    if (typeof title != 'string' || title.trim() === '') {
+      throw new Error('Title must be a non-empty string');
+    }
+    this.#title = title;
+  }
+
+  getDescription() {
+    return this.#description;
+  }
+  setDescription(description) {
+    if (typeof description != 'string' || description.trim() === '' || description.length < 4) {
+      throw new Error('Description must be a non-empty string of more than 4 chars');
+    }
+    this.#description = description;
+  }
+  
+  getDueDate() {
+    return this.#dueDate;
+  }
+  setDueDate(dueDate) {
+    if (!(dueDate instanceof Date) || isNaN(dueDate)) {
+      throw new Error('dueDate must be a valid Date object');
+    }
+    this.#dueDate = dueDate;
+  }
+
+  getPriority() {
+    return this.#priority;
+  }
+  setPriority(priority) {
+    if (typeof priority !== 'boolean') {
+      throw new Error('Priority must be a boolean.');
+    }
+    this.#priority = priority;
+  }
+
+  getDone() {
+    return this.#done;
+  }
+  setDone(done) {
+    if (typeof done !== 'boolean') {
+      throw new Error('Done must must be a boolean.');
+    }
+    this.#done = done;
+  }
+}
+
+export default TodoItem;
